dd37c8570e61b0f50d8779896216655d97f40435
brian-bernal <brian.bernal13@stjohns.edu>
2014-10-28 14:52:07 -0400

    Cleaned up and commented code. Happy to say for large numbers my code is fantastic and much better than brute force. At lower numbers it still does well.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,85 +1,69 @@
-//Ok, I'm starting to get the hang of it. Commit must be local changes, then I push it to the repository. Pull is getting the latest from the repository. Still unsure what publish and sync do.
+/* Running time is .34 Milliseconds
+*  Faster than pure brute force! (Especially at larger numbers)
+*/
+
 package com.company;
 
-import java.math.BigInteger;
-
 public class Main {
-
-    static int primeDiv = 1;
-    static int largestPF = 1;
+    static long largestPF = 1L;
     final static long literal = 600851475143L;
-    final static int[] primes = {827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();  //To find runtime
 
         Main myMain = new Main();
-        //System.out.println(myMain.isPrime(literal));
-        //System.out.println(literal);
-        //for (int e : primes) {
-        long startTime = System.nanoTime();
-        System.out.println(literal + " is prime: " + myMain.isPrime(literal) + " and the largest PF is: " + largestPF);
-        //}
+        System.out.println(literal + " is prime? " + myMain.isPrime(literal) + "\nand the largest PF is: " + largestPF);
+
         long endTime = System.nanoTime();
-        System.out.println("Took "+(endTime - startTime) + " ns");
+        System.out.println("Took " + (endTime - startTime) + " ns");  //Printing runtime
     }
 
     public boolean isPrime(long num) {
-        String number = num + "";
-        int lastDigit = Integer.parseInt(number.charAt(number.length() - 1) + "");  //Last digit
         boolean prime = false;
+        String number = num + "";
+        int lastDigit = Integer.parseInt(number.charAt(number.length() - 1) + "");  //Last digit (faster way to do this?)
 
-//ok
-        if (num == 7 || num == 2 || num == 3 || num == 5)
+        if (num == 2 || num == 3 || num == 5)  //Since 2 is a prime number I have to do this comparison somewhere, same for 3 (3 is divisible by 3, but it could be the largest prime factor) and 5.
             prime = true;
-        else if (lastDigit % 2 == 0)   //if number ends in 0, 2, 4, 6, or 8 it is even and not prime
-        {
+        else if ((lastDigit & 1) == 0) { //If number ends in 0, 2, 4, 6, or 8 it is even and not prime
             prime = false;
             isPrime(num / 2);
-        } else if (lastDigit % 5 == 0)//if number is divisible by 5
-        {
+        } else if (lastDigit - 5 == 0) { //If number is divisible by 5
             prime = false;
-            if (largestPF < 5)
-                largestPF = 5;
             isPrime(num / 5);
-        } else if (divThree(number))   //if the sum of the digits in a number is divisible by 3 it isn't prime
-        {
+        } else if (divThree(number)) {   //If the number is divisible by 3 it isn't prime
             prime = false;
             isPrime(num / 3);
-        } else if (Math.sqrt(num) % 1 == 0)   //If num is the square root of integers it isn't prime
+        } else if (Math.sqrt(num) % 1 == 0)  //If num is the square root of integers it isn't prime
             prime = false;
         else if (isPrimeDivisible(num))  //If num is divisible by a prime it isn't prime
             prime = false;
         else
             prime = true;
+
         if (prime) {
             if (num > largestPF)
-                largestPF = (int)num;//largest factor shouldn't go beyond 2billion as that would be close to largest long
+                largestPF = num;
         }
-
-//if not prime, factor to find greatest prime factor
-//factor function
         return prime;
     }
 
-    public boolean isPrimeDivisible(long num) {
-        int max = (int) Math.sqrt(num);
-        for (int i = 7; i < max; i += 2) {
-           // if (isPrime(i)) {
-                if (num % i == 0) {
-                    if (i > largestPF)
-                        largestPF = i;
-                    //primeDiv = num/i;
-                    isPrime(num/i);
-                    return true;
-             //   }
+    public boolean isPrimeDivisible(long num) {  //If the number ends in a 1, 3, or 7 it could be prime or divisible by a prime
+        int max = (int) Math.sqrt(num);  //Unfortunately must take the square root again
+        for (int i = 7; i < max; i += 2) {  //Starts at 7 because it is the first prime number we haven't accounted for. Increment by 2, because we handled all even numbers already
+            if (num % i == 0) {
+                if (i > largestPF)
+                    largestPF = i;
+                isPrime(num / i);
+                return true;
             }
         }
         return false;
     }
 
-    public boolean divThree(String number) {
+    public boolean divThree(String number) {  //If the sum of the digits in a number is divisible by 3, then the number is divisible by 3 and isn't prime
         long sum = 0;
-        for (int i = 0; i < number.length(); i++) {  //if the sum of the digits in a number is divisible by 3 it is divisible by 3 and isn't prime
+        for (int i = 0; i < number.length(); i++) {  //Would it be faster to convert to character array?
             sum += Integer.parseInt(number.charAt(i) + "");
         }
         return (sum % 3 == 0);
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,19 +1,14 @@
 package com.company;
-
 /**
- * Created by bbern793 on 10/27/2014.
+ * Brute force technique is slower!
  */
 
-//Testing a brute force method to see how long it would take
-    //Wow it's 340 times faster
-
-public class Alt {
-
+public class Test {
     public static void main(String[] args) {
         long literal = 600851475143L;
-        long divisor = 2;
+        int divisor = 2;
         long startTime = System.nanoTime();
-//I'm going to check if the last digit ends in an even number. if so, i will do a bitwise operation. perhaps this is faster than modulus a high number?
+
         while(literal > 1){
             if(0 == (literal % divisor)){
                 literal /= divisor;
@@ -21,7 +16,8 @@ public class Alt {
             }
             divisor++;
         }
-        System.out.println("Largest Prime Factor is: " + divisor);
+        System.out.println(divisor);
+
         long endTime = System.nanoTime();
         System.out.println("Took "+(endTime - startTime) + " ns");
     }
